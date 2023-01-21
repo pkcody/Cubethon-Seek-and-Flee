1999abc4d221695c8d8e910f1775754960301b6c
pkcody <77869251+pkcody@users.noreply.github.com>
2023-01-21 09:59:52 -0700

    first attempt

---
+++ Cubethon Project/Assets/Scripts/AI Steering/SteeringOutput.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringOutput
+{
+    //    class SteeringOutput :
+    //2 linear: Vector
+    //3 angular: float
+
+
+    public Vector3 linear;
+    public float angular;
+}
+
+
+
+public class KinematicSteeringOutput
+{
+    //        class KinematicSteeringOutput :
+    //2 velocity: Vector
+    //rotation: float
+
+    public Vector3 velocity;
+    public float rotation;
+}
